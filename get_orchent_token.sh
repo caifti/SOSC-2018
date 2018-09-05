fb21af1366343c44a361f6b71e83b0c42a72ec1b
dciangot <diego.ciangottini@cern.ch>
2018-09-05 12:19:26 +0200

    part1.a working

---
+++ get_orchent_token.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+IAM_CLIENT_ID=dodas-demo
+IAM_CLIENT_SECRET=dodas-demo
+IAM_USER=***REMOVED***
+
+IAM_CLIENT_ID=${IAM_CLIENT_ID:-iam-client}
+IAM_CLIENT_SECRET=${IAM_CLIENT_SECRET}
+
+echo -ne "Password:"
+stty -echo
+read IAM_PASSWORD
+stty echo
+
+echo
+
+res=$(curl -s -L \
+  -d client_id=${IAM_CLIENT_ID} \
+  -d client_secret=${IAM_CLIENT_SECRET} \
+  -d grant_type=password \
+  -d username=${IAM_USER} \
+  -d password=${IAM_PASSWORD} \
+  -d scope="openid profile email offline_access" \
+  ${IAM_ENDPOINT:-https://dodas-iam.cloud.cnaf.infn.it/token} |  jq '.access_token')
+
+
+if [ $? != 0 ]; then
+  echo "Error!"
+  exit 1
+fi
+
+echo -e "Orchent access token has to be set with the following: \n export ORCHENT_TOKEN=$res"
